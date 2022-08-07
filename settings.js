2213589a27ed9d754ee0cc0e1cdcdbb1930cfa22
Turan Həsənov <89601136+Qasan21@users.noreply.github.com>
2022-08-07 14:42:39 +0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,17 +19,17 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['+994775500382']
+global.premium = ['+994775500382']
+global.ownernomer = '+994775500382'
+global.ownername = 'Ⲧυʀⲁⲛ 🇯🇵'
+global.botname = 'Ⲧυʀⲁⲛ 🇯🇵'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
+global.packname = 'Ⲧυʀⲁⲛ 🇯🇵'
 global.author = '🐦Star Boy Nexus'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
